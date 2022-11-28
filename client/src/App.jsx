adfef4e47f0bb9b83e49ea6fd5591ddd71cac36e
Dave <dczhuo1388@gmail.com>
2022-11-27 23:41:10 -0400

    Channel container edited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelContainer, ChannelListContainer, Auth } from './components';
 // css imports
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
 const cookies = new Cookies();
@@ -28,6 +29,10 @@ if(authToken){
 
 const App = () => {
 
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEdting, setIsEditng] = useState(false);
+
   // if we're not logged in, render the Auth component
   if(!authToken) return <Auth />
 
@@ -36,10 +41,17 @@ const App = () => {
         {/* Pass the client as an instance to the Chat component */}
         <Chat client={client} theme="team light" >
             <ChannelListContainer 
-
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditng={setIsEditng}
             />
             <ChannelContainer 
-            
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              isEdting={isEdting}
+              setIsEditng={setIsEditng}
+              createType={createType}
             />
         </Chat>
     </div>
--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { Channel, useChatContext } from 'stream-chat-react';
 
-const ChannelContainer = () => {
-  return (
-    <div>
-        ChannelContainer
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage} from './';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType,}) => {
+ //gets information about the channel we are currently in
+  const { channel } = useChatContext();
+  
+  //if we are creating a channel, render the CreateChannel component
+  if(isCreating) {
+    return(
+        <div className="channel__container">
+          <CreateChannel createType={createType} setIsCreating={setIsCreating}/>
         </div>
+    )
+  }
+  //if we are editing a channel render the EditChannel component
+  if(isEditing) {
+    return(
+      <div className="channel__container">
+        <EditChannel setIsEditing={setIsEditing}/>
+      </div>
+    )
+  }
+
+  const EmptyState = () => (
+    <div className="channel-empty__container">
+      <p className="channel-empty__first">
+        This is the beginning of your chat history.
+      </p>  
+      <p className="channel-empty__second">
+        Send messages, attachments, links, emojis, and more!
+      </p> 
+    </div>
+  )
+
+  return (
+    <div className='channel__container'>
+        <Channel
+        //if there are no messages in the channel, render the EmptyState component
+          EmptyStateIndicator={EmptyState}
+          Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+        >
+          <ChannelInner setIsEditing={setIsEditing}/>
+        </Channel>
+    </div>
   );
 }
 
