10874660f309589221d93e365a26a41cab0d8b94
Dave <dczhuo1388@gmail.com>
2022-12-01 00:45:35 -0400

    fixed user add function

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,42 +1,65 @@
-import React, { useState } from 'react'
-import {useChatContext} from 'stream-chat-react';
+import React, { useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
 
 import { UserList } from './';
 import { CloseCreateChannel } from '../assets';
 
-//channel name input
-const ChannelNameInput = ({ channelName = '', setChannelName}) => {
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+     //selected users field and adding self to the list
+    const handleChange = (event) => {
+        event.preventDefault();
+        setChannelName(event.target.value);
+    }
 
-  const handleChange = (event) => {
-    // prevents page from refreshing on form submit
-    event.preventDefault();
-    setChannelName(event.target.value);
-  }
-
-  return(
-    <div className='channel-name-input__wrapper'>
-      <p>Name</p>
-      <input value={channelName} onChange={handleChange} placeholder='channel-name' />
-      <p>Add Members</p>
-    </div>
-  )
+    return (
+        <div className="channel-name-input__wrapper">
+            <p>Name</p>
+            <input value={channelName} onChange={handleChange} placeholder="channel-name" />
+            <p>Add Members</p>
+        </div>
+    )
 }
 
-const CreateChannel = ({createType, setIsCreating}) => {
-  
-  const [channelName, setChannelName] = useState('');
-  
-  return (
-    <div className='create-channel__container'>
-      {/* div for creating a new channel */}
-        <div className='create-channel__header'>
-          <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
-          <CloseCreateChannel setIsCreating={setIsCreating} />
+const CreateChannel = ({ createType, setIsCreating }) => {
+    const { client, setActiveChannel } = useChatContext();
+    const [selectedUsers, setSelectedUsers] = useState([client.userID || ''])
+    const [channelName, setChannelName] = useState('');
+
+    const createChannel = async (e) => {
+        e.preventDefault();
+
+        //try catch block to create channel and add members
+        try {
+            const newChannel = await client.channel(createType, channelName, {
+                name: channelName, members: selectedUsers
+            });
+            //set the channel as active channel
+            await newChannel.watch();
+
+            setChannelName('');
+            setIsCreating(false);
+            setSelectedUsers([client.userID]);
+            setActiveChannel(newChannel);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+      // div for creating a new channel
+        <div className="create-channel__container">
+            <div className="create-channel__header">
+                <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
+                <CloseCreateChannel setIsCreating={setIsCreating} />
+            </div>
+            {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
+            <UserList setSelectedUsers={setSelectedUsers} />
+            <div className="create-channel__button-wrapper" onClick={createChannel}>
+                {/* Checks what create type needs to be made  */}
+                <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+            </div>
         </div>
-        {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
-        <UserList />
-    </div>
-  )
+    )
 }
 
 export default CreateChannel
